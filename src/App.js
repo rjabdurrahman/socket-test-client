992dfa08e341f3d3ef46872589ef5def207ca541
rjabdurrahman <md.rahman0296@gmail.com>
2022-12-12 18:33:45 +0600

    Init: Socket.io client

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import io from 'socket.io-client';
+
+const socket = io('http://localhost:3000');
 
 function App() {
+  const [isConnected, setIsConnected] = useState(socket.connected);
+  useEffect(() => {
+    socket.on('connect', () => {
+      setIsConnected(true);
+    });
+
+    socket.on('disconnect', () => {
+      setIsConnected(false);
+    });
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="w3-container w3-teal">
+        <h1>BD Chat</h1>
+      </div>
+      <div>{isConnected ? 'Online' : 'Offline'}</div>
+    </>
   );
 }
 
