a04c1f753d107869eba0b9717783d03776bf7be4
rjabdurrahman <md.rahman0296@gmail.com>
2022-12-12 20:10:01 +0600

    Added: Send message and update messages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import { useEffect, useState } from 'react';
+import { useFormik } from 'formik';
+import { useState } from 'react';
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:3000');
+const socket = io(process.env.REACT_APP_SOCKET_SOURCE);
 
 function App() {
   const [isConnected, setIsConnected] = useState(socket.connected);
   const [messages, setMessages] = useState([]);
-  useEffect(() => {
-    socket.on('connect', () => {
-      setIsConnected(true);
-    });
+  socket.on('connect', () => {
+    setIsConnected(true);
+  });
 
-    socket.on('disconnect', () => {
-      setIsConnected(false);
-    });
+  socket.on('disconnect', () => {
+    setIsConnected(false);
+  });
 
-    socket.on('messages', (messages) => {
-      setMessages(messages)
-    })
+  socket.on('messages', (messages) => {
+    setMessages(messages)
+  })
+
+  socket.on('message', (msg) => {
+    console.log(msg)
+    setMessages([...messages, msg])
+  })
+
+  const {
+    values,
+    errors,
+    handleChange,
+    handleSubmit
+  } = useFormik({
+    initialValues: {
+      msg: ''
+    },
+    validate: values => {
+      const errors = {};
+      if (!values.msg) {
+        errors.msg = 'Message is empty';
+      }
+      return errors;
+    },
+    onSubmit: values => {
+      socket.emit('message', values.msg);
+      values.msg = '';
+    },
   })
   return (
     <>
@@ -33,12 +59,23 @@ function App() {
             user,
             text
           }) => <div key={id}>
-              {time.slice(-10, -5)} - 
-              {user} - 
+              {time.slice(-10, -5)} -
+              {user} -
               {text}
             </div>)
         }
       </div>}
+      <form onSubmit={handleSubmit}>
+        <input
+          className="w3-input"
+          type="text"
+          name="msg"
+          onChange={handleChange}
+          value={values.msg}
+        />
+        {errors.msg && <span className="w3-red">{errors.msg}</span>}<br />
+        <button className="w3-button w3-teal" type="submit">Send</button>
+      </form>
     </>
   );
 }
